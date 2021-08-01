efd7fc6ca661044af5b219ef46b384decae0533d
Nicolas Golob <nicolasgolob@gmail.com>
2021-08-01 04:25:12 +0200

    add disconnect button

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import firebase from '../utils/firebaseConfig';
 
 export default function Main() {
     return (
-        <div>
-            <h1>Hi there ! You are connected !</h1>
-        </div>
+        <>
+            <nav>
+                <h1>Hi There</h1>
+                <h4>Bonjour {firebase.auth().currentUser.displayName}</h4>
+                {/* firebase google doc */}
+                <button onClick={()=>firebase.auth().signOut()}>Disconnect</button>
+            </nav>
+        </>
     )
 }
